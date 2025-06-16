f0c26cce5a154e235724f1118f6c3a97ba47887a
GAUTHIER Remi <gauthierem@groupe-mb.net>
2025-06-16 16:06:24 +0200

    cors config test

---
+++ src/main/java/spring/ai/majordome/spring_ai_majordome/configs/CorsConfig.java
@@ -0,0 +1,18 @@
+package spring.ai.majordome.spring_ai_majordome.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://majordome.netlify.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
